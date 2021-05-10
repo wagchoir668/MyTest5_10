fb712ba807571fe236e9389a4403e81ea23b400d
诚谦 <chengqian.wc@alibaba-inc.com>
2021-05-10 17:03:59 +0800

    666888

--- src/main/java/com/alibaba/c2m/MyTest1.java
+++ src/main/java/com/alibaba/c2m/MyTest1.java
@@ -3,11 +3,22 @@ package com.alibaba.c2m;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author wagchoir
+ */
 public class MyTest1 {
     public static void main(String[] args) {
         System.out.println(111);
         List<Integer> list = new ArrayList<>();
         list.add(7);
         list.add(72);
+
+        User user = new User();
+        String i = "aaa";
+    }
+
+    public static int f1(){
+        System.out.println("666");
+        return 6;
     }
 }
--- src/main/java/com/alibaba/c2m/User.java
+++ src/main/java/com/alibaba/c2m/User.java
@@ -1,2 +1,18 @@
-package com.alibaba.c2m;public class User {
+package com.alibaba.c2m;
+
+public class User {
+    private int id;
+    private String name;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    void ge(){
+
+    }
 }
